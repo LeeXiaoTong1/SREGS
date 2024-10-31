261c58d25b49ee8500589f5c54eba8a9b668f0a1
LeeXiaoTong1 <130888073+LeeXiaoTong1@users.noreply.github.com>
2024-10-31 16:02:26 +0800

    Update train_360.py

--- train_360.py
+++ train_360.py
@@ -177,15 +177,7 @@ def training(dataset, opt, pipe, args, depth_model):
             args.depth_weight = 0.001
 
         patch_range = (5, 16)
-        depth_n = render_pkg["depth"][0].unsqueeze(0)
-        anyth_n = midas_depth.unsqueeze(0)
-        #         print(depth_n.shape)
-        #         print(anyth_n.shape)
-
-        anyth_n = 255.0 - anyth_n
-
-        loss_l2_dpt = patch_norm_mse_loss(depth_n[None, ...], anyth_n[None, ...],
-                                          randint(patch_range[0], patch_range[1]), opt.error_tolerance)
+     
         loss += 0.01 * loss_l2_dpt
 
         loss.backward()
